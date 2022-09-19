11323ddd46e37173ec9510999102b36799742980
Umut Deniz Darendeli <108900524+UmizDemud@users.noreply.github.com>
2022-09-19 08:09:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,776 @@
+var rawFollowers = `bangbengbang'in profil resmi
+bangbengbang
+Benginur İlkar
+denizoztunc'in profil resmi
+denizoztunc
+Deniz
+eddemirel'in profil resmi
+eddemirel
+eda
+dogan.yenal'in profil resmi
+dogan.yenal
+Doğan Mert Yenal
+emreoneerrr'in profil resmi
+emreoneerrr
+Emre Öner
+canberkorunc'in profil resmi
+canberkorunc
+Kadir Canberk
+aybikcansn'in profil resmi
+aybikcansn
+Aybike Cansın
+bariskbalkan'in profil resmi
+bariskbalkan
+Barış Kaan Balkan
+altar_k'in profil resmi
+altar_k
+Altar
+unalyilmazzz'in profil resmi
+unalyilmazzz
+Av. Ünal Yılmaz
+ceyhuncelik'in profil resmi
+ceyhuncelik
+Ceyhun Çelik
+tarkydn'in profil resmi
+tarkydn
+Tarik
+silaayikk'in profil resmi
+silaayikk
+Sıla Ayık
+canberksackan'in profil resmi
+canberksackan
+CanberkSackan
+burcinacksz'in profil resmi
+burcinacksz
+𝐵𝑢𝑟𝑐𝑖𝑛
+brkybrb'in profil resmi
+brkybrb
+Berkay
+mertsonmezgul'in profil resmi
+mertsonmezgul
+Mert Sönmezgül
+tolgagklp'in profil resmi
+tolgagklp
+Tolga Gökalp
+ygtberkk'in profil resmi
+ygtberkk
+Yiğit Berk ŞAHİN
+ozgurkaank'in profil resmi
+ozgurkaank
+özgür kaan kaya
+ariferdim'in profil resmi
+ariferdim
+sezer_fzk'in profil resmi
+sezer_fzk
+Sezer Turşucu
+emrecanerbas'in profil resmi
+emrecanerbas
+Emre Can Erbaş
+aylinceeereeen'in profil resmi
+aylinceeereeen
+Ceren
+orhnaliozdgn'in profil resmi
+orhnaliozdgn
+Orhan Ali ÖZDOĞAN
+m_berke_o'in profil resmi
+m_berke_o
+Murat Berke Oktay
+ardacanaksoy'in profil resmi
+ardacanaksoy
+arda can aksoy
+utkuzun22'in profil resmi
+utkuzun22
+Duran Utku Uzun
+isilhocek'in profil resmi
+isilhocek
+burakkgenc'in profil resmi
+burakkgenc
+Burak Genç
+onuurakkaya'in profil resmi
+onuurakkaya
+Onur Akkaya
+azizcanyilmz'in profil resmi
+azizcanyilmz
+Aziz Can Yılmaz
+emrealper.ea'in profil resmi
+emrealper.ea
+Emre Alper
+erguyagmurr'in profil resmi
+erguyagmurr
+Yagmur Ergu
+sercanomay'in profil resmi
+sercanomay
+Sercan OMAY
+melikeparmaksiz'in profil resmi
+melikeparmaksiz
+av.semihacar'in profil resmi
+av.semihacar
+Semih Acar
+kemaltuzz'in profil resmi
+kemaltuzz
+Kemal TUZ
+ali.cangunduz'in profil resmi
+ali.cangunduz
+bbusraaydogdu'in profil resmi
+bbusraaydogdu
+Büşra Aydoğdu
+simaytunckilic'in profil resmi
+simaytunckilic
+ege.xyz'in profil resmi
+ege.xyz
+Ege Mutlu
+bahutongal'in profil resmi
+bahutongal
+Bahu Tongal
+berkaysamalar'in profil resmi
+berkaysamalar
+Berkay Samalar
+selinn.yasarr'in profil resmi
+selinn.yasarr
+selin yaşar
+d_korkmazturk'in profil resmi
+d_korkmazturk
+Doğukan Korkmaztürk
+v.hansa'in profil resmi
+v.hansa
+Hansa
+yankiseven'in profil resmi
+yankiseven
+Yankı Seven
+yldzahmett'in profil resmi
+yldzahmett
+Ahmet Yildiz
+bikembulak'in profil resmi
+bikembulak
+Bikem
+dt_ilaydacelik'in profil resmi
+dt_ilaydacelik
+ilayda çelik
+atty.bilgeasena'in profil resmi
+atty.bilgeasena
+Bilge Asena Süeren
+gulcinaciksoz'in profil resmi
+gulcinaciksoz
+Gülçin Açıksöz
+sevvall.karaca'in profil resmi
+sevvall.karaca
+şevval
+bahadir.senturk'in profil resmi
+bahadir.senturk
+Bahadır Şentürk
+secil.itisken'in profil resmi
+secil.itisken
+Seçil
+mausanmaz'in profil resmi
+mausanmaz
+𝙈𝙚𝙝𝙢𝙚𝙩 𝘼𝙡𝙞 𝙐𝙎𝘼𝙉𝙈𝘼𝙕
+ani.gunduzbey'in profil resmi
+ani.gunduzbey
+Anı Gündüzbey
+nursanakinci'in profil resmi
+nursanakinci
+Nursan Akıncı
+teacher_of_math_geo'in profil resmi
+teacher_of_math_geo
+Ö𝔃𝓰𝓮 Ö𝓩𝓓𝓔𝓜 ツ
+gurkandogan94'in profil resmi
+gurkandogan94
+Gurkan Dogan
+fasilar'in profil resmi
+fasilar
+Furkan Aşılar
+av.dogutuncer'in profil resmi
+av.dogutuncer
+Doğu Tuncer
+eaytug'in profil resmi
+eaytug
+Emre Aytuğ Dön
+gozdesarikaya'in profil resmi
+gozdesarikaya
+gozdesarikaya
+benguolmeez'in profil resmi
+benguolmeez
+Bengü Ölmez
+selenonder'in profil resmi
+selenonder
+Selen
+tolga.aktas'in profil resmi
+tolga.aktas
+Tolga Aktas
+ersanburan'in profil resmi
+ersanburan
+Erşan Buran
+gracechinh5983'in profil resmi
+gracechinh5983
+·
+Grace Anouk
+ece.ksel'in profil resmi
+ece.ksel
+ece yüksel
+alee.e.e'in profil resmi
+alee.e.e
+ALI🔥REZa
+kaannoncu'in profil resmi
+kaannoncu
+Kaan Öncü
+aesatugur'in profil resmi
+aesatugur
+tekdemiridil'in profil resmi
+tekdemiridil
+İdil Tekdemir
+nabican_gokturk_ozdemir'in profil resmi
+nabican_gokturk_ozdemir
+Göktürk Özdemir
+hakankesekler199'in profil resmi
+hakankesekler199
+Hakan Kesekler
+oykudemirell'in profil resmi
+oykudemirell
+oykk
+arslanaykutcan'in profil resmi
+arslanaykutcan
+Aykut Can Arslan
+eren.etci'in profil resmi
+eren.etci
+Eren Etci
+oraykt'in profil resmi
+oraykt
+Oray Kurt
+aslii.merr'in profil resmi
+aslii.merr
+aslicerentekk'in profil resmi
+aslicerentekk
+·
+Aslı Ceren TEKIŞIK
+irfanajj'in profil resmi
+irfanajj
+İrfan
+cankoktekin'in profil resmi
+cankoktekin
+Can Koktekin
+mrtelbasan'in profil resmi
+mrtelbasan
+Mert Elbasan
+mertcetin8'in profil resmi
+mertcetin8
+Mert Çetin
+seraaykavukk'in profil resmi
+seraaykavukk
+Seray Kavuk
+ebuaygun'in profil resmi
+ebuaygun
+Ebu
+idkurtoglusefa'in profil resmi
+idkurtoglusefa
+Sefa Kurtoğlu
+gamzesevimlii'in profil resmi
+gamzesevimlii
+Gamze Sevimli
+fatihkznn'in profil resmi
+fatihkznn
+Fatih Kozan
+sizendeniz'in profil resmi
+sizendeniz
+berrkpeker'in profil resmi
+berrkpeker
+Berk Peker
+zykrgl'in profil resmi
+zykrgl
+Zeynep Kirgil
+atacankullabci'in profil resmi
+atacankullabci
+atajuan
+kithogue'in profil resmi
+kithogue
+Nadia
+yobroalvin'in profil resmi
+yobroalvin
+Amit
+goksueroll'in profil resmi
+goksueroll
+Göksu Erol
+egehavuroglu'in profil resmi
+egehavuroglu
+·
+Ege
+seyf00o'in profil resmi
+seyf00o
+SeyFo Başkurt
+selenisz'in profil resmi
+selenisz
+Selen Sansar
+kaanakartepe'in profil resmi
+kaanakartepe
+Kaan Akartepe
+muratonell'in profil resmi
+muratonell
+Murat Önel
+hakangenc1'in profil resmi
+hakangenc1
+Hakan Genç
+leonard_patterson_'in profil resmi
+leonard_patterson_
+Leonard Patterson
+onur_cglyn'in profil resmi
+onur_cglyn
+Onur Çağlayan
+mr.saribudak'in profil resmi
+mr.saribudak
+Ali Tahsin
+aetoraman'in profil resmi
+aetoraman
+Ali Erdem Toraman
+ata_kirac'in profil resmi
+ata_kirac
+Atakan KIRAÇ
+ayboyey'in profil resmi
+ayboyey
+AYBERK YILDIRIM
+berfinsuyayci'in profil resmi
+berfinsuyayci
+Berfin Su Yaycı
+burakunkun'in profil resmi
+burakunkun
+Burak UNKUN
+busebile'in profil resmi
+busebile
+Buse Bile
+deniz.topfisenk'in profil resmi
+deniz.topfisenk
+Deniz
+ekin.darendeli'in profil resmi
+ekin.darendeli
+Ekin
+savarfatih'in profil resmi
+savarfatih
+Fatih Savar
+alexbingol'in profil resmi
+alexbingol
+İskender Yağız Bingöl
+uffukcakirr'in profil resmi
+uffukcakirr
+Ufuk Çakır
+zeynepgurrkan'in profil resmi
+zeynepgurrkan
+·
+Zeynep Gürkan
+`;
+
+var rawFollowing = `ece.ksel'in profil resmi
+ece.ksel
+ece yüksel
+selenisz'in profil resmi
+selenisz
+Selen Sansar
+dt_ilaydacelik'in profil resmi
+dt_ilaydacelik
+ilayda çelik
+sizendeniz'in profil resmi
+sizendeniz
+tolga.aktas'in profil resmi
+tolga.aktas
+Tolga Aktas
+goksueroll'in profil resmi
+goksueroll
+Göksu Erol
+onur_cglyn'in profil resmi
+onur_cglyn
+Onur Çağlayan
+oraykt'in profil resmi
+oraykt
+Oray Kurt
+ersanburan'in profil resmi
+ersanburan
+Erşan Buran
+muratonell'in profil resmi
+muratonell
+Murat Önel
+aslii.merr'in profil resmi
+aslii.merr
+nabican_gokturk_ozdemir'in profil resmi
+nabican_gokturk_ozdemir
+Göktürk Özdemir
+ekin.darendeli'in profil resmi
+ekin.darendeli
+Ekin
+benguolmeez'in profil resmi
+benguolmeez
+Bengü Ölmez
+atacankullabci'in profil resmi
+atacankullabci
+atajuan
+nursanakinci'in profil resmi
+nursanakinci
+Nursan Akıncı
+zykrgl'in profil resmi
+zykrgl
+Zeynep Kirgil
+berfinsuyayci'in profil resmi
+berfinsuyayci
+Berfin Su Yaycı
+gozdesarikaya'in profil resmi
+gozdesarikaya
+gozdesarikaya
+ani.gunduzbey'in profil resmi
+ani.gunduzbey
+Anı Gündüzbey
+seyf00o'in profil resmi
+seyf00o
+SeyFo Başkurt
+selenonder'in profil resmi
+selenonder
+Selen
+irfanajj'in profil resmi
+irfanajj
+İrfan
+eaytug'in profil resmi
+eaytug
+Emre Aytuğ Dön
+berrkpeker'in profil resmi
+berrkpeker
+Berk Peker
+hakangenc1'in profil resmi
+hakangenc1
+Hakan Genç
+ata_kirac'in profil resmi
+ata_kirac
+Atakan KIRAÇ
+eren.etci'in profil resmi
+eren.etci
+Eren Etci
+gamzesevimlii'in profil resmi
+gamzesevimlii
+Gamze Sevimli
+mr.saribudak'in profil resmi
+mr.saribudak
+Ali Tahsin
+savarfatih'in profil resmi
+savarfatih
+Fatih Savar
+uffukcakirr'in profil resmi
+uffukcakirr
+Ufuk Çakır
+idkurtoglusefa'in profil resmi
+idkurtoglusefa
+Sefa Kurtoğlu
+aetoraman'in profil resmi
+aetoraman
+Ali Erdem Toraman
+oykudemirell'in profil resmi
+oykudemirell
+oykk
+leonard_patterson_'in profil resmi
+leonard_patterson_
+Leonard Patterson
+ania__ao'in profil resmi
+ania__ao
+Ania
+deniz.topfisenk'in profil resmi
+deniz.topfisenk
+Deniz
+lorakulaksiz'in profil resmi
+lorakulaksiz
+Larisa Kulaksiz
+busebile'in profil resmi
+busebile
+Buse Bile
+ayboyey'in profil resmi
+ayboyey
+AYBERK YILDIRIM
+bahadir.senturk'in profil resmi
+bahadir.senturk
+Bahadır Şentürk
+seraaykavukk'in profil resmi
+seraaykavukk
+Seray Kavuk
+burakunkun'in profil resmi
+burakunkun
+Burak UNKUN
+mertcetin8'in profil resmi
+mertcetin8
+Mert Çetin
+gurkandogan94'in profil resmi
+gurkandogan94
+Gurkan Dogan
+secil.itisken'in profil resmi
+secil.itisken
+Seçil
+ebuaygun'in profil resmi
+ebuaygun
+Ebu
+yobroalvin'in profil resmi
+yobroalvin
+Amit
+chloehmal_art'in profil resmi
+chloehmal_art
+Chloe Mal
+kaannoncu'in profil resmi
+kaannoncu
+Kaan Öncü
+mrtelbasan'in profil resmi
+mrtelbasan
+Mert Elbasan
+cankoktekin'in profil resmi
+cankoktekin
+Can Koktekin
+bikembulak'in profil resmi
+bikembulak
+Bikem
+gulcinaciksoz'in profil resmi
+gulcinaciksoz
+Gülçin Açıksöz
+evazhang12'in profil resmi
+evazhang12
+张益华
+mausanmaz'in profil resmi
+mausanmaz
+𝙈𝙚𝙝𝙢𝙚𝙩 𝘼𝙡𝙞 𝙐𝙎𝘼𝙉𝙈𝘼𝙕
+fasilar'in profil resmi
+fasilar
+Furkan Aşılar
+yakuptechin'in profil resmi
+yakuptechin
+Yakup Tekin
+yankiseven'in profil resmi
+yankiseven
+Yankı Seven
+sevvall.karaca'in profil resmi
+sevvall.karaca
+şevval
+arslanaykutcan'in profil resmi
+arslanaykutcan
+Aykut Can Arslan
+d_korkmazturk'in profil resmi
+d_korkmazturk
+Doğukan Korkmaztürk
+arjun_sahajpal'in profil resmi
+arjun_sahajpal
+Arjun sahajpal
+av.dogutuncer'in profil resmi
+av.dogutuncer
+Doğu Tuncer
+teacher_of_math_geo'in profil resmi
+teacher_of_math_geo
+Ö𝔃𝓰𝓮 Ö𝓩𝓓𝓔𝓜 ツ
+tekdemiridil'in profil resmi
+tekdemiridil
+İdil Tekdemir
+kaanakartepe'in profil resmi
+kaanakartepe
+Kaan Akartepe
+gkalkanli'in profil resmi
+gkalkanli
+Gülten Kalkanlı
+kithogue'in profil resmi
+kithogue
+Nadia
+aesatugur'in profil resmi
+aesatugur
+fatihkznn'in profil resmi
+fatihkznn
+Fatih Kozan
+atty.bilgeasena'in profil resmi
+atty.bilgeasena
+Bilge Asena Süeren
+alexbingol'in profil resmi
+alexbingol
+İskender Yağız Bingöl
+yldzahmett'in profil resmi
+yldzahmett
+Ahmet Yildiz
+selinn.yasarr'in profil resmi
+selinn.yasarr
+selin yaşar
+v.hansa'in profil resmi
+v.hansa
+Hansa
+ekinakkayaa'in profil resmi
+ekinakkayaa
+Ekin Akkaya
+ata.kan_kirac'in profil resmi
+ata.kan_kirac
+Atakan Kıraç
+melikeparmaksiz'in profil resmi
+melikeparmaksiz
+dogan.yenal'in profil resmi
+dogan.yenal
+Doğan Mert Yenal
+erguyagmurr'in profil resmi
+erguyagmurr
+Yagmur Ergu
+bahutongal'in profil resmi
+bahutongal
+Bahu Tongal
+isilhocek'in profil resmi
+isilhocek
+kemaltuzz'in profil resmi
+kemaltuzz
+Kemal TUZ
+denizoztunc'in profil resmi
+denizoztunc
+Deniz
+emrecanerbas'in profil resmi
+emrecanerbas
+Emre Can Erbaş
+tolgagklp'in profil resmi
+tolgagklp
+Tolga Gökalp
+utkuzun22'in profil resmi
+utkuzun22
+Duran Utku Uzun
+simaytunckilic'in profil resmi
+simaytunckilic
+ygtberkk'in profil resmi
+ygtberkk
+Yiğit Berk ŞAHİN
+azizcanyilmz'in profil resmi
+azizcanyilmz
+Aziz Can Yılmaz
+canberksackan'in profil resmi
+canberksackan
+CanberkSackan
+ali.cangunduz'in profil resmi
+ali.cangunduz
+orhnaliozdgn'in profil resmi
+orhnaliozdgn
+Orhan Ali ÖZDOĞAN
+tarkydn'in profil resmi
+tarkydn
+Tarik
+ardacanaksoy'in profil resmi
+ardacanaksoy
+arda can aksoy
+m_berke_o'in profil resmi
+m_berke_o
+Murat Berke Oktay
+silaayikk'in profil resmi
+silaayikk
+Sıla Ayık
+eddemirel'in profil resmi
+eddemirel
+eda
+aybikcansn'in profil resmi
+aybikcansn
+Aybike Cansın
+ceyhuncelik'in profil resmi
+ceyhuncelik
+Ceyhun Çelik
+ege.xyz'in profil resmi
+ege.xyz
+Ege Mutlu
+sezer_fzk'in profil resmi
+sezer_fzk
+Sezer Turşucu
+onuurakkaya'in profil resmi
+onuurakkaya
+Onur Akkaya
+sercanomay'in profil resmi
+sercanomay
+Sercan OMAY
+aylinceeereeen'in profil resmi
+aylinceeereeen
+Ceren
+brkybrb'in profil resmi
+brkybrb
+Berkay
+av.semihacar'in profil resmi
+av.semihacar
+Semih Acar
+bariskbalkan'in profil resmi
+bariskbalkan
+Barış Kaan Balkan
+canberkorunc'in profil resmi
+canberkorunc
+Kadir Canberk
+bangbengbang'in profil resmi
+bangbengbang
+Benginur İlkar
+ozgurkaank'in profil resmi
+ozgurkaank
+özgür kaan kaya
+bbusraaydogdu'in profil resmi
+bbusraaydogdu
+Büşra Aydoğdu
+emrealper.ea'in profil resmi
+emrealper.ea
+Emre Alper
+burcinacksz'in profil resmi
+burcinacksz
+𝐵𝑢𝑟𝑐𝑖𝑛
+altar_k'in profil resmi
+altar_k
+Altar
+berkaysamalar'in profil resmi
+berkaysamalar
+Berkay Samalar
+hakankesekler199'in profil resmi
+hakankesekler199
+Hakan Kesekler
+mertsonmezgul'in profil resmi
+mertsonmezgul
+Mert Sönmezgül
+algan.mert'in profil resmi
+algan.mert
+Algan Mert Yaşar
+dt.nurbanueroglu'in profil resmi
+dt.nurbanueroglu
+Diş Hekimi Nurbanu Eroğlu
+unalyilmazzz'in profil resmi
+unalyilmazzz
+Av. Ünal Yılmaz
+religionmuseum'in profil resmi
+religionmuseum
+Museum Of History Of Religion
+`;
+
+var followers = [];
+let match;
+var linesFollowers = rawFollowers.split("\n");
+
+for (let i = 0; i < linesFollowers.length; i++) {
+  match = linesFollowers[i].match(/(?!\n).+(?='in profil resmi)/g);
+
+  if (match) {
+    followers.push(match[0]);
+  }
+}
+
+var linesFollowing = rawFollowing.split("\n");
+
+var following = [];
+
+for (let i = 0; i < linesFollowing.length; i++) {
+  match = linesFollowing[i].match(/(?!\n).+(?='in profil resmi)/g);
+  if (match) {
+    following.push(match[0]);
+  }
+}
+
+var mismatch =
+  "<b>" +
+  "Number of followers: " +
+  "</b>" +
+  followers.length +
+  "<br><b>" +
+  "Number of following: " +
+  "</b>" +
+  following.length;
+
+var faund = false;
+var count = 1;
+for (let i = 0; i < following.length; i++) {
+  faund = false;
+  for (let j = 0; j < followers.length; j++) {
+    if (following[i] === followers[j]) {
+      faund = true;
+      break;
+    }
+  }
+
+  if (!faund) {
+    mismatch += "<br>" + count + ".) " + following[i];
+    count++;
+  }
+}
+
+document.getElementById("app").innerHTML = mismatch;
